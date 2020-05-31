20bf4db1ee8b6876d78b27af2a74b9b15ad0514a
Jake Krayger <jkrayg@protonmail.com>
2020-05-31 09:29:55 -0400

    fixing travis

--- models/example.js
+++ models/example.js
@@ -1,12 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
-  var Products = sequelize.define("products", {
-    name: DataTypes.STRING,
-    price: DataTypes.DECIMAL,
-    inventory: DataTypes.INTEGER,
-    rating: DataTypes.INTEGER,
-    
-  }, {
-    timestamps: false
-  })
+  var Products = sequelize.define(
+    "products",
+    {
+      name: DataTypes.STRING,
+      price: DataTypes.DECIMAL,
+      inventory: DataTypes.INTEGER,
+      rating: DataTypes.INTEGER
+    },
+    {
+      timestamps: false
+    }
+  );
   return Products;
 };
